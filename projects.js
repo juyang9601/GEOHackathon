e6386d77e22291d5854f179e9889a36d263d37d1
juyang9601 <44347235+juyang9601@users.noreply.github.com>
2021-10-03 05:49:38 -0400

    Add files via upload

---
+++ projects.js
@@ -0,0 +1,13 @@
+var projectsData = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+
+"features": [
+  { "type": "Feature", "properties": { "Latitude": -1.4170261, "Longitude": -77.4446936, "ID": 1, "Link": "https:\/\/wrc.osu.edu\/sites\/default\/files\/uploads\/gouma_2018oh568b_finalreport.pdf" }, "geometry": { "type": "Point", "coordinates": [ -77.4446936, -1.4170261 ] } },
+  { "type": "Feature", "properties": { "Latitude": -1.5808158, "Longitude": -77.7223162, "ID": 2, "Link": "https:\/\/wrc.osu.edu\/sites\/default\/files\/uploads\/gouma_2018oh568b_finalreport.pdf" }, "geometry": { "type": "Point", "coordinates": [ -77.7223162, -1.5808158 ] } },
+  { "type": "Feature", "properties": { "Latitude": -1.1683258, "Longitude": -77.3999161, "ID": 3, "Link": "https:\/\/wrc.osu.edu\/sites\/default\/files\/uploads\/gouma_2018oh568b_finalreport.pdf" }, "geometry": { "type": "Point", "coordinates": [ -77.3999161, -1.1683258 ] } },
+  { "type": "Feature", "properties": { "Latitude": -1.3574403, "Longitude": -77.1884669, "ID": 4, "Link": "https:\/\/wrc.osu.edu\/sites\/default\/files\/uploads\/gouma_2018oh568b_finalreport.pdf" }, "geometry": { "type": "Point", "coordinates": [ -77.1884669, -1.3574403 ] } },
+  { "type": "Feature", "properties": { "Latitude": -1.929594, "Longitude": -77.4173297, "ID": 5, "Link": "https:\/\/wrc.osu.edu\/sites\/default\/files\/uploads\/gouma_2018oh568b_finalreport.pdf" }, "geometry": { "type": "Point", "coordinates": [ -77.4173297, -1.929594 ] } },
+
+]
+}
---
+++ test.js
@@ -0,0 +1,12 @@
+projectsData={
+"type": "FeatureCollection",
+"name": "test1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "ID": 10032, "longitude": -1.4170261, "latitude": -77.4446936, "links": "https://drive.google.com/file/d/1PBKjssDW_SPUI4D4hZFFcmSP-DVNC1XN/view?usp=sharing" }, "geometry": { "type": "Point", "coordinates": [ -77.4446936, -1.4170261 ] } },
+{ "type": "Feature", "properties": { "ID": 10042, "longitude": -1.5808158, "latitude": -77.7223162, "links": "https://drive.google.com/file/d/1PBKjssDW_SPUI4D4hZFFcmSP-DVNC1XN/view?usp=sharing" }, "geometry": { "type": "Point", "coordinates": [ -77.7223162, -1.5808158 ] } },
+{ "type": "Feature", "properties": { "ID": 30045, "longitude": -1.1683258, "latitude": -77.3999161, "links": "https://drive.google.com/file/d/1PBKjssDW_SPUI4D4hZFFcmSP-DVNC1XN/view?usp=sharing" }, "geometry": { "type": "Point", "coordinates": [ -77.3999161, -1.1683258 ] } },
+{ "type": "Feature", "properties": { "ID": 43321, "longitude": -1.3574403, "latitude": -77.1884669, "links": "https://drive.google.com/file/d/1PBKjssDW_SPUI4D4hZFFcmSP-DVNC1XN/view?usp=sharing" }, "geometry": { "type": "Point", "coordinates": [ -77.1884669, -1.3574403 ] } },
+{ "type": "Feature", "properties": { "ID": 50003, "longitude": -1.929594, "latitude": -77.4173297, "links": "https://drive.google.com/file/d/1PBKjssDW_SPUI4D4hZFFcmSP-DVNC1XN/view?usp=sharing" }, "geometry": { "type": "Point", "coordinates": [ -77.4173297, -1.929594 ] } }
+]
+}
